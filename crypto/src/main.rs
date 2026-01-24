f8c6ed9309453da68505ebf819740847bf94d56e
phu.nguyen <phu.nguyen19@gmail.com>
2026-01-24 15:06:43 +0700

    feat: simple CLI with commands options

--- crypto/src/main.rs
+++ crypto/src/main.rs
@@ -1,3 +1,31 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(name="shamir", version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Say hello to someone
+    Hello {
+        #[arg(short, long)]
+        name: String,
+    },
+    /// Say goodbye to someone
+    Goodbye {
+        #[arg(short, long)]
+        name: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Hello { name } => println!("Hello, {}!", name),
+        Commands::Goodbye { name } => println!("Goodbye, {}!", name),
+    }
 }
