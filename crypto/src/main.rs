6fb02b4a093c130ec2afcf9b43494c39b6d0f639
Phu Nguyen <phunguyen@RPN-Mac.local>
2026-02-11 08:16:16 +0700

    feat: improve tests

--- crypto/src/main.rs
+++ crypto/src/main.rs
@@ -415,10 +415,10 @@ mod tests {
 
     #[test]
     fn test_fewer_than_k_should_not_build_secret() {
-        let test_cases: Vec<(usize, usize, usize, Option<Vec<BigInt>>)> = vec![
-            (5, 3, 2, Some(vec![166.into(), 94.into()])),
-            (5, 5, 4, None),
-            (5, 2, 1, None),
+        let test_cases: Vec<(usize, usize, Option<Vec<BigInt>>)> = vec![
+            (5, 3, Some(vec![166.into(), 94.into()])),
+            (5, 5, None),
+            (5, 2, None),
         ];
         for t in test_cases {
             let generate_result = generate_share(GenerateShareParams {
@@ -426,10 +426,10 @@ mod tests {
                 shares: t.0,
                 threshold: t.1,
                 prime: 1613.into(),
-                coefficients: t.3,
+                coefficients: t.2,
             })
             .unwrap();
-            for val in subsets(&generate_result.shares, t.2) {
+            for val in subsets(&generate_result.shares, t.1 - 1) {
                 let reconstruct_result = reconstruct(ReconstructParams {
                     shares: val,
                     prime: 1613.into(),
@@ -442,10 +442,11 @@ mod tests {
 
     #[test]
     fn test_greater_than_k_should_build_secret() {
-        let test_cases: Vec<(usize, usize, usize, Option<Vec<BigInt>>)> = vec![
-            (5, 3, 2, Some(vec![166.into(), 94.into()])),
-            (5, 4, 4, None),
-            (5, 2, 1, None),
+        let test_cases: Vec<(usize, usize, Option<Vec<BigInt>>)> = vec![
+            (5, 2, None),
+            (5, 3, Some(vec![166.into(), 94.into()])),
+            (6, 4, None),
+            (7, 5, None),
         ];
         for t in test_cases {
             let generate_result = generate_share(GenerateShareParams {
@@ -453,12 +454,12 @@ mod tests {
                 shares: t.0,
                 threshold: t.1,
                 prime: 1613.into(),
-                coefficients: t.3,
+                coefficients: t.2,
             })
             .unwrap();
-            for val in subsets(&generate_result.shares, t.2 + 1) {
+            for val in subsets(&generate_result.shares, t.1 + 1) {
                 let reconstruct_result = reconstruct(ReconstructParams {
-                    shares: val,
+                    shares: val.clone(),
                     prime: 1613.into(),
                 })
                 .unwrap();
@@ -634,7 +635,7 @@ mod tests {
             let generate_result = generate_share(GenerateShareParams {
                 secret: 1234.into(),
                 threshold: test_case.1,
-                shares: test_case.1.clone(),
+                shares: test_case.1,
                 prime: 1613.into(),
                 coefficients: Some(test_case.2.clone()),
             });
