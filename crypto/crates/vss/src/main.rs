f81bd1f35c53eecb2ea49a9b64e768e42e3b3ec7
Phu Nguyen <phunguyen@192.168.1.197>
2026-03-11 08:33:39 +0700

    feat: crypto package: vss deal function, validation

--- crypto/crates/vss/src/main.rs
+++ crypto/crates/vss/src/main.rs
@@ -172,6 +172,7 @@ struct DealParams {
     coeffs_g: Vec<BigUint>,
 }
 
+#[derive(Debug)]
 struct DealResult {
     // E_i
     commitments: Vec<BigUint>,
@@ -179,7 +180,107 @@ struct DealResult {
     shares: Vec<(usize, BigUint, BigUint)>,
 }
 
+fn deal(params: DealParams) -> Result<DealResult, String> {
+    // validate players is valid
+    // validate threshold is valid and smaller than players
+    if params.threshold < 2 || params.threshold > params.players {
+        return Err(format!(
+            "threshold must be greater than 1 and smaller than number of players {}, receive {}",
+            &params.players, &params.threshold
+        ));
+    }
+    // validate prime valid
+    if !is_prime(&params.prime) {
+        return Err(format!("prime value {} is not valid", params.prime));
+    }
+    // validate order valid
+    if !is_prime(&params.order)
+        || &params.prime % &params.order != 1_u8.into()
+        || params.order >= params.prime
+    {
+        return Err(format!(
+            "order value {} is not valid for prime {}",
+            params.order, params.prime
+        ));
+    }
+    // validate g valid
+    if params.generator_g.modpow(&params.order, &params.prime) != 1_u8.into() {
+        return Err(format!("generator g {} is not valid", params.generator_g));
+    }
+    // validate h valid
+    if params.generator_h.modpow(&params.order, &params.prime) != 1_u8.into() {
+        return Err(format!("generator h {} is not valid", params.generator_h));
+    }
+    // validate coeffs f and g equal len
+    if params.coeffs_f.len() != params.coeffs_g.len() {
+        return Err(format!(
+            "F and G must have the same number for coeffcients. Receives F coeffs {} G coeffs {}",
+            params.coeffs_f.len(),
+            params.coeffs_g.len(),
+        ));
+    }
+    // validate coeffs f and g valid
+    for coeff in [params.coeffs_f, params.coeffs_g].concat() {
+        if coeff < 1_u8.into() || coeff > params.order {
+            return Err(format!(
+                "coefficients must not smaller than 1 and greater than order {}, receive {}",
+                params.order, coeff
+            ));
+        }
+    }
+
+    return Err("".into());
+}
+
+fn is_prime(n: &BigUint) -> bool {
+    let two: BigUint = 2_u8.into();
+
+    if n < &two {
+        return false;
+    }
+    if n == &two {
+        return true;
+    }
+    if n % &two == BigUint::ZERO {
+        return false;
+    }
+
+    let mut i: BigUint = 3_u8.into();
+    while &(&i * &i) <= n {
+        if n % &i == BigUint::ZERO {
+            return false;
+        }
+        i += &two;
+    }
+    true
+}
+
 fn main() {
     let args = Cli::parse();
     println!("debug command: {:?}", args);
+    match args.command {
+        Commands::Deal {
+            players,
+            threshold,
+            pogh,
+            debug_coeffs,
+        } => {
+            if let Some(poghv) = pogh {
+                if let Some(debug_coeffs_v) = debug_coeffs {
+                    let r = deal(DealParams {
+                        players,
+                        threshold,
+                        prime: poghv.prime,
+                        order: poghv.order,
+                        generator_g: poghv.generator_g,
+                        generator_h: poghv.generator_h,
+                        coeffs_f: debug_coeffs_v.0,
+                        coeffs_g: debug_coeffs_v.1,
+                    });
+                    println!("{:?}", r);
+                }
+            }
+        }
+        _ => {}
+    }
 }
